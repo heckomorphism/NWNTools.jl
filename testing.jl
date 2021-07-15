36fedd3032b119280ba919264e299de1e343cca5
Emma <emmakjohnson@protonmail.com>
2021-07-15 17:03:48 -0600

    Basic test case

--- testing.jl
+++ testing.jl
@@ -1,8 +1,13 @@
 using NWNTools, StaticArrays, BenchmarkTools, LinearAlgebra, SparseArrays, LightGraphs, SimpleWeightedGraphs
-dims = SA[1.0,1.0]
-lines = [Line(SA[0.0,0.5],SA[1.0,0.5]),Line(SA[0.5,0.0],SA[0.5,0.5])]
-wprops = repeat([WireProp(22.0,0.02)],length(lines))
-elecs = [Line(SA[0.0,0.0],SA[0.0,1.0]),Line(SA[0.0,0.0],SA[1.0,0.0])]
-volts = [5.0,3.3]
-grnds = [Line(SA[1.0,0.0],SA[1.0,1.0])]
-nwn = NWN_JDA(lines, wprops, elecs, volts, grnds, dims, 7.0,11.0)
+dims = SA[20.0,20.0]
+ρₙ = 0.4
+l = 7.0
+ρₑ = 22.2
+D = 0.02
+V₊ = 5.0
+Rⱼ = 11.0
+Rₑ = 7.0
+
+nwn = rand_network_JDA(dims, round(Int,prod(dims)*ρₙ), l, ρₑ, D, V₊, Rⱼ, Rₑ)
+
+e = ensemble_JDA(100, dims, ρₙ, l, ρₑ, D, V₊, Rⱼ, Rₑ)
